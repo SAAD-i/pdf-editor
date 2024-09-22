df7d8d1cabe4d75e513669cf51504187893c078c
Muhammad Saad <102303434+SAAD-i@users.noreply.github.com>
2024-09-22 21:04:40 +0500

    Update layout.js

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -15,12 +15,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <UserProvider>
-          <div className="relative -z-50 h-full w-full bg-slate-950">
-            <div class="fixed bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+          <div className="bg-slate-950">
+            <Navbar />
+            <Toaster position="top-right" />
+            {children}
           </div>
-          <Navbar />
-          <Toaster position="top-right" />
-          {children}
         </UserProvider>
       </body>
     </html>
